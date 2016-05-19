6cce60db2ffcaae141158013dad1db440412ab30
Johannes Seikowsky <seikowsky@googlemail.com>
2016-05-20 00:59:36 +0300

    learning interface edited

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,21 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+function reveal(ger, eng){
+  ger = document.getElementById(ger).style;
+  eng = document.getElementById(eng).style;
+      
+  if(ger.display == 'none' || ger.display == ''){
+    ger.display = 'block';     
+  }else{
+    ger.display = 'none';
+  }
+ 
+  if(eng.display == 'block' || eng.display == ''){
+    eng.display = 'none';     
+  }else{
+    eng.display = 'block';
+  }
+}
