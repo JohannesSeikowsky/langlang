b7c2eefed54b1722ea3da70d7fe78136d7d6ff77
Johannes Seikowsky <seikowsky@googlemail.com>
2016-05-25 21:40:32 +0300

    basic authorization and no word repeat in the same session

--- app/controllers/views_controller.rb
+++ app/controllers/views_controller.rb
@@ -1,17 +1,35 @@
 class ViewsController < ApplicationController
 
+  before_action :record_words_in_session, only: [:learning]
+
   def home
   end
 
   def learning
-    if session[:id]
-      session[:words] ||= []
+    # make sure user is logged in
+    if current_user
+      # next Word and make sure it hasnt already been asked for in this session  
       @word = Word.get_random
-      session[:words] << @word.english
-      render "learning"
+      if session[:words].include?(@word.english)
+        # only continue if not all Words have been covered in session to avoid infinite loop crash. 
+        if session[:words].count << Word.count
+          redirect_to learning_path
+        else
+          root_with_notice("you have covered all words in this course. thank you.")
+        end
+      else
+        # save word in current session and generate the view.
+        session[:words] << @word.english
+        render "learning"     
+      end
+
     else
       root_with_notice("please log in.")
     end
+  end
+
+  def record_words_in_session
+    session[:words] ||= []
   end
 
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -2,6 +2,7 @@ class Word < ActiveRecord::Base
 
   def self.get_random
     order("RANDOM()").first
+    # Word.find(rand(1..Word.count))
   end
 
 end
